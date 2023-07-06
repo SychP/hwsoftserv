e997941ff080361d5212dc2bd001ad05b7a5b40d
SychP <kr483108768@gmail.com>
2023-07-06 12:00:14 +0100

    add var3.sh

---
+++ var3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dir_path=~/softserv/
+file_path=${dir_path}test_file.txt
+
+echo "Creating file $file_path"
+touch $file_path
+
+echo "Checking content of dir $dir_path"
+ls $dir_path
+
