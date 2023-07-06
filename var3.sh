f742977985dcb4574618a16057475133227982b8
SychP <kr483108768@gmail.com>
2023-07-06 12:09:12 +0100

    changed var3.sh

--- var3.sh
+++ var3.sh
@@ -3,6 +3,13 @@
 dir_path=~/softserv/
 file_path=${dir_path}test_file.txt
 
+
+if [ ! -d "$dir_path" ]; then
+  mkdir "$dir_path"
+  echo "Folder $dir_path created."
+  chmod 755 "$dir_path"
+fi
+
 echo "Creating file $file_path"
 touch $file_path
 
